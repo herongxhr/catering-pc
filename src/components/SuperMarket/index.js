e71993b83fd413f1e9fc88e03df21223ff31de75
何荣 <herongxhr@163.com>
2019-02-19 17:08:06 +0800

    合并

--- src/components/SuperMarket/index.js
+++ src/components/SuperMarket/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Axios from '../../axios'
-import { Card,Icon,Button } from 'antd'
+import { Card, Icon, Button } from 'antd'
 import Clicker from '../Clicker'
 import './index.less'
 
@@ -8,40 +8,40 @@ const { Meta } = Card;
 
 class MarketCommodity extends React.Component {
   state = {
-    commodity:[]
+    commodity: []
   }
   componentDidMount() {
     Axios.ajax({
-      url:'/supermarket'
+      url: '/supermarket'
     }).then((value) => {
       this.setState({
-        commodity:value
+        commodity: value
       })
       console.log(this.state.commodity)
     })
   }
 
   render() {
-    return(
+    return (
       <div className='gallery'>
         {
-          this.state.commodity.map(item => 
-          <Card
-            hoverable
-            style={{ width:278,height:375 }}
-            key={item.key}
-            actions={[<Clicker style={{width:114}}/>,<Button type='primary'>加入购物车</Button>]}
-          >
-            <Meta
-              title={item.product}
-              description={item.company}
-            />
-          </Card>
+          this.state.commodity.map(item =>
+            <Card
+              hoverable
+              style={{ width: 278, height: 375 }}
+              key={item.key}
+              actions={[<Clicker style={{ width: 114 }} />, <Button type='primary'>加入购物车</Button>]}
+            >
+              <Meta
+                title={item.product}
+                description={item.company}
+              />
+            </Card>
           )
         }
       </div>
     )
   }
-} 
+}
 
 export default MarketCommodity
