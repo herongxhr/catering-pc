f54a63512d13701763ecb0e77dcc7873e900678d
何荣 <herongxhr@163.com>
2019-02-20 09:25:42 +0800

    增加server统一发送请求

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ const { Content, Footer } = Layout;
 
 class App extends Component {
   render() {
-    const style = {
-      width: 1200,
-      margin: '0 auto',
-    }
+    // const style = {
+    //   width: 1200,
+    //   margin: '0 auto',
+    // }
     return (
       <Layout className="layout">
         <Jheader />
-        <Content style={style}>
+        <Content /*style={style}*/>
           {this.props.children}
         </Content>
       </Layout>
---
+++ src/services/api.js
@@ -0,0 +1,3 @@
+//https://github.com/ljharb/qs
+//qs.stringify(object, [options]);
+import { stringify } from 'qs';
