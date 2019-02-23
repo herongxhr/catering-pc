dad0761f9311b5b7516ab128323056948ba586b3
何荣 <herongxhr@163.com>
2019-02-23 09:35:34 +0800

    合并

--- src/components/TodayMenuCard/TodayMenuCard.js
+++ src/components/TodayMenuCard/TodayMenuCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './TodayMenuCard.less';
-import { Empty} from 'antd';
+import { Empty } from 'antd';
 
 class TodayMenuCard extends Component {
   render() {
@@ -13,28 +13,28 @@ class TodayMenuCard extends Component {
       <div className="MenuCard">
         <div className='title'>今日菜单</div>
         <div className='todaymenu'>
-            <div className='breakfast'>
-              <div>早餐</div>
-              <ul>
-                {
-                  (breakfast) ?
-                  breakfast.map(data => <li key={data.index}>{data}</li>) : <Empty
-                  image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
-                  description={
-                    <span>
-                      暂未排餐哦~
+          <div className='breakfast'>
+            <div>早餐</div>
+            <ul>
+              {
+                (breakfast) ?
+                  breakfast.map((data, index) => <li key={index}>{data}</li>) : <Empty
+                    image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
+                    description={
+                      <span>
+                        暂未排餐哦~
                     </span>
-                  }
-                />
-                }
-              </ul>
-            </div>
-            <div className='lunch'>
-              <div>中餐</div>
-              <ul>
-                  {
-                    (lunch) ?
-                    lunch.map(data => <li key={data.index}>{data}</li>) : <Empty 
+                    }
+                  />
+              }
+            </ul>
+          </div>
+          <div className='lunch'>
+            <div>中餐</div>
+            <ul>
+              {
+                (lunch) ?
+                  lunch.map((data, index) => <li key={index}>{data}</li>) : <Empty
                     image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
                     description={
                       <span>
@@ -42,43 +42,43 @@ class TodayMenuCard extends Component {
                       </span>
                     }
                   />
-                  }
-                </ul>
-            </div>
-            <div className='dessert'>
-              <div>点心</div>
-              <ul>
-                {
-                  (dessert) ?
-                  (dessert.map(data => <li key={data.index}>{data}</li>)) : (<Empty
-                  image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
-                  description={
-                    <span >
-                      暂未排餐哦~
+              }
+            </ul>
+          </div>
+          <div className='dessert'>
+            <div>点心</div>
+            <ul>
+              {
+                (dessert) ?
+                  (dessert.map((data, index) => <li key={index}>{data}</li>)) : (<Empty
+                    image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
+                    description={
+                      <span >
+                        暂未排餐哦~
                     </span>
-                  }
-                />)
-                }
-              </ul>
-            </div>
-            <div className='dinner'>
-              <div>晚餐</div>
-              <ul>
-                {
-                  (dinner) ?
-                  dinner.map(data => <li key={data.index}>{data}</li>) : <Empty
-                  image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
-                  description={
-                    <span >
-                      暂未排餐哦~
+                    }
+                  />)
+              }
+            </ul>
+          </div>
+          <div className='dinner'>
+            <div>晚餐</div>
+            <ul>
+              {
+                (dinner) ?
+                  dinner.map((data, index) => <li key={index}>{data}</li>) : <Empty
+                    image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
+                    description={
+                      <span >
+                        暂未排餐哦~
                     </span>
-                  }
-                />
-                }
-              </ul>
-            </div>
+                    }
+                  />
+              }
+            </ul>
+          </div>
         </div>
-        
+
       </div>
     );
   }
