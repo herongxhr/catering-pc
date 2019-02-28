5eab49d1467a87144061d4a4a8f49041f2fdb3fd
何荣 <herongxhr@163.com>
2019-02-28 18:43:12 +0800

    2月28日全并

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -3,13 +3,9 @@ import './index.less';
 import TodayMenuCard from '../../components/TodayMenuCard/TodayMenuCard'
 import TodoListCard from '../../components/TodoListCard/TodoListCard'
 import Accepting from '../../components/Accepting/Accepting'
-<<<<<<< HEAD:src/pages/index/index.js
-import { Card, Button, Tabs,Radio,Divider } from 'antd';
+import { Card, Button, Tabs, Radio, Divider } from 'antd';
 import { Link } from 'react-router-dom';
-=======
-import { Card, Button, Tabs,Radio,Table } from 'antd';
 import Charts from 'ant-design-pro/lib/Charts';
->>>>>>> 1b56687ab8f0a4b1832feb06923875a273109e50:src/pages/Index/index.js
 import { Pie, yuan } from 'ant-design-pro/lib/Charts';
 import moment from 'moment'
 import { connect } from 'dva';
@@ -21,37 +17,37 @@ const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
 
 class A extends Component {
-  state={
-    tabkey:'today',
-    time:'month',
+  state = {
+    tabkey: 'today',
+    time: 'month',
   }
   queryTodoList = () => {
-		const { dispatch } = this.props;
-		//请求待办事项
-		dispatch({
-			type: 'home/queryTodoLists',
-		})
+    const { dispatch } = this.props;
+    //请求待办事项
+    dispatch({
+      type: 'home/queryTodoLists',
+    })
   }
   querytodayMenu = () => {
-		const { dispatch } = this.props;
-		//请求待办事项
-		dispatch({
-			type: 'home/querytodayMenu',
-		})
+    const { dispatch } = this.props;
+    //请求待办事项
+    dispatch({
+      type: 'home/querytodayMenu',
+    })
   }
   querydeviceInfo = () => {
-		const { dispatch } = this.props;
-		dispatch({
-			type: 'home/querydeviceInfo',
-		})
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'home/querydeviceInfo',
+    })
   }
   queryStatistics = (params = {}) => {
     const { dispatch } = this.props;
     //请求待办事项
     dispatch({
       type: 'home/queryStatistics',
-      payload:{
-        timeType:this.state.time,
+      payload: {
+        timeType: this.state.time,
         ...params
       }
     })
@@ -64,14 +60,14 @@ class A extends Component {
   }
 
   render() {
-    const { home }= this.props
+    const { home } = this.props
     const todoList = home.todoList || [];
     const todayMenu = home.todayMenu || {};
     const deviceInfo = home.deviceInfo || [];
     var timestamp = Date.parse(new Date());
     const date = moment(timestamp).format("YYYY-MM-DD dddd")
     const weeks = moment(timestamp).format("WW")
-    return ( 
+    return (
       <div className="App">
         <div>{this.props.children}</div>
         <div className="App-content">
@@ -84,61 +80,61 @@ class A extends Component {
           </div>
           <div className="App-content-data">
             <div>
-              <TodoListCard todoList = {todoList}/>
+              <TodoListCard todoList={todoList} />
               <div className='tools'>
                 <Card title="常用工具" bordered={false} style={{ width: 350 }}>
-                  <Button className='toolsbtn cgml' onClick={() =>{
+                  <Button className='toolsbtn cgml' onClick={() => {
                     this.props.history.push('/purcatalog')
                   }}>采购目录</Button>
-                  <Button className='toolsbtn' onClick={() =>{
+                  <Button className='toolsbtn' onClick={() => {
                     this.props.history.push('/parameter')
                   }}>本月台账</Button>
-                  <Button className='toolsbtn' onClick={() =>{
+                  <Button className='toolsbtn' onClick={() => {
                     this.props.history.push('/outstock')
                   }}>缺货上报</Button>
                 </Card>
               </div>
             </div>
-            <TodayMenuCard todayMenu={todayMenu}/>
+            <TodayMenuCard todayMenu={todayMenu} />
           </div>
-          <div className="App-content-accepting"> 
-            <Tabs tabBarExtraContent={operations} onChange={(key)=>{this.setState({tabkey:key})}}>
-              <TabPane tab="今日验收" key="today"><Accepting key={this.state.tabkey}/></TabPane>
-              <TabPane tab="明日验收" key="tomorrow"><Accepting key={this.state.tabkey}/></TabPane>
+          <div className="App-content-accepting">
+            <Tabs tabBarExtraContent={operations} onChange={(key) => { this.setState({ tabkey: key }) }}>
+              <TabPane tab="今日验收" key="today"><Accepting key={this.state.tabkey} /></TabPane>
+              <TabPane tab="明日验收" key="tomorrow"><Accepting key={this.state.tabkey} /></TabPane>
             </Tabs>
           </div>
           <div className='App-content-paying-wrapper'>
             <div className='App-content-paying'>
-              <div style={{display:'flex',justifyContent:'space-between'}}>
-                  <p style={{width:226,height:65,fontSize:16,lineHeight:4,rgba:(0,0,0,0.85)}}>应付款统计分析</p>
-                  <div> 
-                    <RadioGroup defaultValue="month" onChange={(e)=>{this.setState({time:e.target.value})}}>
-                      <RadioButton value="month">本月</RadioButton>
-                      <RadioButton value="quarter">本季度</RadioButton>
-                      <RadioButton value="year">本年</RadioButton>
-                    </RadioGroup>
-                  </div>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <p style={{ width: 226, height: 65, fontSize: 16, lineHeight: 4, rgba: (0, 0, 0, 0.85) }}>应付款统计分析</p>
+                <div>
+                  <RadioGroup defaultValue="month" onChange={(e) => { this.setState({ time: e.target.value }) }}>
+                    <RadioButton value="month">本月</RadioButton>
+                    <RadioButton value="quarter">本季度</RadioButton>
+                    <RadioButton value="year">本年</RadioButton>
+                  </RadioGroup>
+                </div>
               </div>
               <div>
-                  <Pie
-                    hasLegend
-                    data={home.statistics}
-                    valueFormat={val => <span dangerouslySetInnerHTML={{ __html: yuan(val) }} />}
-                    height={206}
-                  />
+                <Pie
+                  hasLegend
+                  data={home.statistics}
+                  valueFormat={val => <span dangerouslySetInnerHTML={{ __html: yuan(val) }} />}
+                  height={206}
+                />
               </div>
             </div>
             <div className='App-content-opening'>
               <div className='timeTitle'>设备最后开机时间</div>
-                <Divider />
-                <div className='openingItem'><span>晨检仪</span><span>{deviceInfo.morningDetector}</span></div>
-                <Divider />
-                <div className='openingItem'><span>验货机</span><span>{deviceInfo.inspectionMachine}</span></div>
-                <Divider />
-                <div className='openingItem'><span>易检设备</span><span>{deviceInfo.inspectionEquipment}</span></div>
-                <Divider />
-                <div style={{width:350,height:54,textAlign:'center',marginTop:18,color:'#54C4CE'}}>查看全部</div>
-              </div>
+              <Divider />
+              <div className='openingItem'><span>晨检仪</span><span>{deviceInfo.morningDetector}</span></div>
+              <Divider />
+              <div className='openingItem'><span>验货机</span><span>{deviceInfo.inspectionMachine}</span></div>
+              <Divider />
+              <div className='openingItem'><span>易检设备</span><span>{deviceInfo.inspectionEquipment}</span></div>
+              <Divider />
+              <div style={{ width: 350, height: 54, textAlign: 'center', marginTop: 18, color: '#54C4CE' }}>查看全部</div>
+            </div>
           </div>
         </div>
       </div>
@@ -147,6 +143,6 @@ class A extends Component {
 }
 
 const ShowARouter = withRouter(A);
-export default connect(( {home} ) => ({
+export default connect(({ home }) => ({
   home,
 }))(ShowARouter);
